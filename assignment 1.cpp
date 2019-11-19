c2a1b165226a34e0c7d14fc1e892d7d54ce3ccff
Mission101 <54994213+Mission101@users.noreply.github.com>
2019-11-19 12:00:06 +0800

    Add files via upload

---
+++ assignment 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int a,b,c,d,e,f,g,h,i,j,q;
+cout << "Enter the values of a,b,c,d,e,f,g,h,i and j" <<endl;
+q= a+b+c+d+e+f+g+h+i+j;
+cin>>a;
+cin>>b;
+cin>>c;
+cin>>d;
+cin>>e;
+cin>>f;
+cin>>g;
+cin>>h;
+cin>>i;
+cin>>j;
+cout<<"The sum of a,b,c,d,e,f,g,h,i,j is" <<endl;
+cout<<q;	
+}
